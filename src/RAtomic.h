dd9ad53b1e11beca812f0ce05cee613f0b18402c
Pierre <pierre.blank@lumithd.fr>
2017-06-21 17:34:55 +0200

    Atomic

---
+++ src/RAtomic.h
@@ -0,0 +1,39 @@
+#ifndef __RATOMIC_H__
+#define __RATOMIC_H__
+
+#include "RMutex.h"
+
+namespace RPTR
+{
+  template<typename T>
+  class Atomic
+  {
+  public:
+    Atomic(T val) : m_data(val) {}
+
+    T get()
+    {
+      T ret;
+
+      m_mut.lock();
+      ret = m_data;
+      m_mut.unlock();
+      return ret;
+    }
+
+    void operator=(T &val)
+    {
+      m_mut.lock();
+      m_data = val;
+      m_mut.unlock();
+    }
+
+  private:
+    T     m_data;
+    Mutex m_mut;
+  };
+}
+
+
+
+#endif /* !__RATOMIC_H__ */
--- src/RThread.h
+++ src/RThread.h
@@ -9,16 +9,16 @@ namespace RPTR
     {
     public:
         Thread();
-        Thread(void (* funct)(void *), void* param);
+        Thread(void (*funct)(void *), void* param);
         void start(void (*funct)(void *), void *param);
         void join();
         void detach();
-        
+
         ~Thread();
     private:
         bool    m_joinable;
         struct RThread_data;
-        
+
         std::unique_ptr<RThread_data>   m_data;
     };
 }
